670b33ed5b14d2557bc330d3f3a7a09f0cccde1f
Zhyzhang1998 <93493602+Zhyzhang1998@users.noreply.github.com>
2024-08-09 15:52:58 -0400

    fix: styles

--- src/components/Styles.jsx
+++ src/components/Styles.jsx
@@ -1,57 +1,50 @@
-import { colors } from "@mui/material";
-
 const styles = {
-  navButton: {
-    mx: 2,
-    color: "#333333",
-    fontFamily: "poppins",
-    fontWeight: 600,
+  navButton: { 
+    mx: 2, 
+    color: '#333333',
+    fontFamily: 'poppins',
+    fontWeight:600,
     fontSize: 16,
-    textTransform: "None",
-    "&:hover": {
-      backgroundColor: "#F6F6F6",
-    },
+    textTransform: 'None',
+    '&:hover': {
+      backgroundColor: '#F6F6F6',
+    }
   },
   regularButton: {
-    color: "white",
-    fontFamily: "poppins",
-    fontWeight: 600,
+    color:'white',
+    fontFamily: 'poppins',
+    fontWeight:600,
     fontSize: 16,
-    textTransform: "None",
-    backgroundColor: "#333333",
-    border: "solid #333333 2px",
-    "&:hover": {
-      color: "#333333",
-      backgroundColor: "#ffffff",
-      border: "solid #747474 2px",
-    },
+    textTransform: 'None',
+    backgroundColor: '#333333',
+    border: 'solid #333333 2px',
+    '&:hover': {color:'#333333', backgroundColor: '#ffffff', border: 'solid #747474 2px'}
   },
   gridSetting: {
-  logo: {
-    width: "100%",
-    maxHeight: "35px",
-    paddingLeft: "30px",
-    paddingRight: "30px",
-  },
-  footer: {
-    backgroundColor: "#B2C2EB",
-  },
-  formSetting: {
-    display: "grid",
-    gap: "15px",
-    gridTemplateColumns: "repeat(12, 1fr)",
-    px: "120px",
-  },
-};
     display: 'grid',
     gap: '15px',
     gridTemplateColumns: 'repeat(12, 1fr)',
   },
-  containerStyle: {
-    px: '120px',
-    paddingTop:'128px',
-    paddingBottom: '64px'
+    logo: {
+      width: "100%",
+      maxHeight: "35px",
+      paddingLeft: "30px",
+      paddingRight: "30px",
+    },
+    footer: {
+      backgroundColor: "#B2C2EB",
+    },
+    formSetting: {
+      display: "grid",
+      gap: "15px",
+      gridTemplateColumns: "repeat(12, 1fr)",
+      px: "120px",
+    },
+    containerStyle: {
+      px: '120px',
+      paddingTop:'128px',
+      paddingBottom: '64px'
+    }
   }
-}
 
 export default styles;
